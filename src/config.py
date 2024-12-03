6d2681884c243b7aea07f859780e579fdc80769d
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-03 16:40:25 +0100

    fix: new main and config

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+    POSTMARK_API_TOKEN: str
+    BASE_URL: str
+    FRONTEND_BASE_URL: str
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
---
+++ src/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from src.database import Base, engine, get_db, close_connector
+from src.user.router import user_router
+from google.cloud import bigquery
+from fastapi.responses import JSONResponse
+# Creates app instance
+app = FastAPI()
+
+# CORS configuration
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:53023",  # Add frontend origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Specify frontend origin
+    allow_credentials=False,  # Allow cookies and credentials if needed
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Include user router
+app.include_router(user_router, prefix="/api/user",
+                   tags=["User and Authentication"])
+
+# Initialize database models
+Base.metadata.create_all(bind=engine)
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    # Log the error (optional)
+    print(f"Unhandled error: {exc}")
+
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "An internal server error occurred."},
+        # Add CORS headers
+        headers={"Access-Control-Allow-Origin": "http://localhost:5173"},
+    )
+
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+    return {
+        "status": "success",
+        "msg": "Hello from a public endpoint! You don't need to be authenticated to see this."
+    }
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    # Close the connector when the application shuts down
+    close_connector()
+
+
+@app.get("/api/db")
+def read_root(db: Session = Depends(get_db)):
+    # Example query to verify the database connection
+    result = db.execute("SELECT 1").fetchall()
+    return {"result": result}
+
+
+@app.get("/test/db")
+async def run_query():
+    query = """
+    SELECT * FROM `aiap-436610.comp_gee_data.process`
+    LIMIT 10
+    """
+    try:
+        # Initialize BigQuery client
+        client = bigquery.Client.from_service_account_json(
+            'private-key-bigQuery-account.json')
+        query_job = client.query(query)  # Make API request
+        results = query_job.result()  # Wait for job to complete
+
+        # Convert the results to a list of dictionaries
+        print(results)
+        return [dict(row) for row in results]
+    except Exception as e:
+        return {"error": str(e)}
