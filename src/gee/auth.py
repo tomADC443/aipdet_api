bc2f05e22d9910ee6f9620a451dd5bcba183ac3a
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-03 16:41:06 +0100

    fix: new gee private key

--- src/gee/auth.py
+++ src/gee/auth.py
@@ -5,5 +5,5 @@ import os
 def authenticate():
     service_account = "gee-675@aiap-436610.iam.gserviceaccount.com"
     credentials = ee.ServiceAccountCredentials(
-        service_account, os.path.abspath("private-key.json"))
+        service_account, os.path.abspath("private-key-gee-service-account.json"))
     ee.Initialize(credentials)
