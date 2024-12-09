f0607099f4ce976c94bbc4c3f715ec7f55ca5ea9
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-09 09:06:27 +0100

    feat: add task endpoint validation

--- src/aoi/schemas.py
+++ src/aoi/schemas.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel, Field
 from pydantic_geojson import FeatureModel
 
@@ -16,4 +17,34 @@ class AOICreationRequest(BaseModel):
     geometry: FeatureModel = Field(
         ...,
         description="The geometry of the AOI as a GeoJSON Feature object."
+    )
+
+
+class AOI(BaseModel):
+    id: str = Field(
+        ...,
+        description="The unique identifier of the AOI."
+    )
+    name: str = Field(
+        ...,
+        description="The name of the AOI."
+    )
+    description: str = Field(
+        None,
+        description="An optional description of the AOI."
+    )
+    createdAt: int = Field(
+        ...,
+        description="The timestamp when the AOI was created."
+    )
+    geometry: FeatureModel = Field(
+        ...,
+        description="The geometry of the AOI as a GeoJSON Feature object."
+    )
+
+
+class AoiGetResponse(BaseModel):
+    aois: List[AOI] = Field(
+        ...,
+        description="A list of AOIs."
     )
---
+++ src/task/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Task_Status(Enum):
+    Successful = "Successful"
+    Failed = "Failed"
+    Processing = "Processing"
