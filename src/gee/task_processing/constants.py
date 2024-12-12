a10ccdb4f462cd8511d008325636592360ce2abf
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-12 17:13:41 +0100

    fix: change timedelta constants from years to days (and reduce timedelta for dev)

--- src/gee/task_processing/constants.py
+++ src/gee/task_processing/constants.py
@@ -1,4 +1,4 @@
 
 # Availability of Sentinel-2 imagery starts at 01.04.2017
-NUMBER_OF_YEARS_TEMPORAL_MAX = 7  # 7 year back
+NUMBER_OF_DAYS_TEMPORAL_MAX = 30  # 7 years back = 2555 days
 DAY_ONLY_FEATURE_LABEL = "REDUCED_LOCAL_DAY"
