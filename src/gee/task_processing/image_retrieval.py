05fb97abb99883d9f3a3b6d282484712f8e0f863
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-12 14:17:06 +0100

    feat: get sentinel imaging for batch processing

---
+++ src/gee/task_processing/image_retrieval.py
@@ -0,0 +1,20 @@
+import ee
+from datetime import datetime
+
+
+def get_imagery(
+    aoi: ee.Geometry.Polygon,
+    start_date: datetime,
+    end_date: datetime,
+) -> ee.ImageCollection:
+
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
+
+    collection = (
+        ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+        .filterDate(start_date_str, end_date_str)
+        .filterBounds(aoi)
+        .filter(ee.Filter.lte("CLOUDY_PIXEL_PERCENTAGE", 95))
+    )
+    return collection
