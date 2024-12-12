59709632ead3817c651e3edd6f3302a828664a95
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-12 17:12:56 +0100

    fix: return image in map functions, type conversion

--- src/gee/task_processing/processing.py
+++ src/gee/task_processing/processing.py
@@ -1,7 +1,7 @@
 import ee
 
 
-def derive_value(image_collection: ee.ImageCollection):
+def process_collection(image_collection: ee.ImageCollection):
 
     image_collection = image_collection.map(add_ndvi)
     image_collection = image_collection.map(generate_valid_area_polygons)
@@ -14,7 +14,7 @@ def add_ndvi(image):
     return image.addBands(ndvi)
 
 
-def generate_valid_area_polygons(image: ee.Image):
+def generate_valid_area_polygons(image: ee.Image) -> ee.Image:
     # Masked areas are considered invalid
     valid_mask = image.mask()
     valid_polygons = valid_mask.reduceToVectors(
@@ -24,6 +24,7 @@ def generate_valid_area_polygons(image: ee.Image):
         bestEffort=True
     )
     image.set('valid_polygons', valid_polygons)
+    return image
 
 
 def generate_ndvi_polygons(image: ee.Image):
@@ -35,14 +36,17 @@ def generate_ndvi_polygons(image: ee.Image):
         bestEffort=True
     )
     image.set('ndvi_polygons', ndvi_polygons)
+    return image
 
 
 def combine_ndvi_and_valid_polygons(image: ee.Image):
     valid_polygons = image.get('valid_polygons')
     ndvi_polygons = image.get('ndvi_polygons')
-    combined_polygons = valid_polygons.map(
+    valid_polygons_feat_coll = ee.FeatureCollection(valid_polygons)
+    ndvi_polygons_feat_coll = ee.FeatureCollection(ndvi_polygons)
+    combined_polygons = valid_polygons_feat_coll.map(
         lambda valid_feature: valid_feature.set(
-            'ndvi_polygons', ndvi_polygons.filterBounds(
+            'ndvi_polygons', ndvi_polygons_feat_coll.filterBounds(
                 valid_feature.geometry())
         )
     )
