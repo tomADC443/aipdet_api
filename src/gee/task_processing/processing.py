cc7ae7ecd94d9471e357e2c4c976836389297fcd
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-16 09:28:39 +0100

    feat: optimization and typing

--- src/gee/task_processing/processing.py
+++ src/gee/task_processing/processing.py
@@ -8,6 +8,8 @@ def process_collection(image_collection: ee.ImageCollection):
     image_collection = image_collection.map(generate_ndvi_polygons)
     image_collection = image_collection.map(combine_ndvi_and_valid_polygons)
 
+    return image_collection
+
 
 def add_ndvi(image):
     ndvi = image.normalizedDifference(['B8', 'B4']).rename('NDVI')
@@ -23,11 +25,10 @@ def generate_valid_area_polygons(image: ee.Image) -> ee.Image:
         scale=10,
         bestEffort=True
     )
-    image.set('valid_polygons', valid_polygons)
-    return image
+    return ee.Image(image.set('valid_polygons', valid_polygons))
 
 
-def generate_ndvi_polygons(image: ee.Image):
+def generate_ndvi_polygons(image: ee.Image) -> ee.Image:
 
     ndvi_polygons = image.select('NDVI').gte(0.6).reduceToVectors(
         geometryType='polygon',
@@ -35,20 +36,26 @@ def generate_ndvi_polygons(image: ee.Image):
         scale=10,
         bestEffort=True
     )
-    image.set('ndvi_polygons', ndvi_polygons)
-    return image
+    return ee.Image(image.set('ndvi_polygons', ndvi_polygons))
 
 
-def combine_ndvi_and_valid_polygons(image: ee.Image):
+def combine_ndvi_and_valid_polygons(image: ee.Image) -> ee.Image:
     valid_polygons = image.get('valid_polygons')
     ndvi_polygons = image.get('ndvi_polygons')
     valid_polygons_feat_coll = ee.FeatureCollection(valid_polygons)
     ndvi_polygons_feat_coll = ee.FeatureCollection(ndvi_polygons)
-    combined_polygons = valid_polygons_feat_coll.map(
-        lambda valid_feature: valid_feature.set(
-            'ndvi_polygons', ndvi_polygons_feat_coll.filterBounds(
-                valid_feature.geometry())
+    combined_polygons = ee.FeatureCollection(valid_polygons_feat_coll.map(
+        combined_polygons=valid_polygons_feat_coll.map(
+            lambda valid_feature: valid_feature.set(
+                'ndvi_polygons', ndvi_polygons_feat_coll.filter(
+                    ee.Filter.Or(
+                        # Fully inside
+                        ee.Filter.within('.geo', valid_feature.geometry()),
+                        # Exactly equal
+                        ee.Filter.equals('.geo', valid_feature.geometry())
+                    )
+                )
+            )
         )
-    )
-    image.set('combined_polygons', combined_polygons)
-    return image
+    ))
+    return ee.Image(image.set('combined_polygons', combined_polygons))
--- src/gee/task_processing/utils.py
+++ src/gee/task_processing/utils.py
@@ -1,14 +1,12 @@
-from src.gee.schemas import PolygonInputSchema
 from timezonefinder import TimezoneFinder
 from shapely.geometry import Polygon
 from src.gee.task_processing.exceptions import TimezoneRetrievalError
 tf = TimezoneFinder()
 
 
-def get_time_zone_of_center_point(aoi: PolygonInputSchema) -> str:
+def get_time_zone_of_center_point(aoi: Polygon) -> str:
 
-    shapely_polygon = Polygon(aoi.coordinates)
-    centroid = shapely_polygon.centroid
+    centroid = aoi.centroid
     time_zone = tf.timezone_at(lng=centroid.x, lat=centroid.y)
     if time_zone:
         return time_zone
