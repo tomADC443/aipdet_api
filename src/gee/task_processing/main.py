4e0a13b55acb10410dde4ca3c8f797b1e34f6460
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-17 10:56:10 +0100

    feat: improve gee pipeline

--- src/gee/task_processing/main.py
+++ src/gee/task_processing/main.py
@@ -1,4 +1,3 @@
-from src.gee.schemas import GeoJSONPolygonFeature
 from datetime import datetime, timedelta
 from src.gee.task_processing.constants import NUMBER_OF_DAYS_TEMPORAL_MAX
 from src.gee.auth import authenticate
@@ -7,12 +6,13 @@ from shapely.geometry import Polygon
 from src.gee.task_processing.image_retrieval import get_imagery
 from src.gee.task_processing.utils import get_time_zone_of_center_point
 from src.gee.task_processing.processing import process_collection
-from pydantic import validate_call
+from src.gee.task_processing.prepare_export import prepare_export
+from src.gee.task_processing.export import start_export
 import ee
+from src.gee.task_processing.metadata import GeeTaskProcessingMetadata
 
 
-@validate_call
-def start_task_process(area_polygon_feature: GeoJSONPolygonFeature):
+def start_task_process(shapely_aoi_polygon: Polygon, metadata: GeeTaskProcessingMetadata):
     """
     Function to process a GeoJSON Polygon input.
 
@@ -23,28 +23,25 @@ def start_task_process(area_polygon_feature: GeoJSONPolygonFeature):
         str: Confirmation message.
     """
 
+    coordinates = list(shapely_aoi_polygon.exterior.coords)
     end_date = datetime.now()
     start_date = end_date - timedelta(days=NUMBER_OF_DAYS_TEMPORAL_MAX)
-    shapely_aoi = Polygon(area_polygon_feature.geometry.coordinates[0])
-
-    time_zone = get_time_zone_of_center_point(shapely_aoi)
-
+    metadata.time_zone = get_time_zone_of_center_point(shapely_aoi_polygon)
     # 1. Initialization - From here on all calculations run on GEE Servers, no local code allowed
     authenticate()
-    aoi = ee.Geometry.Polygon(area_polygon_feature.geometry.coordinates)
+    aoi = ee.Geometry.Polygon(coordinates)
     # 2. Get Imagery
     image_collection = get_imagery(aoi, start_date, end_date)
     # 3. Preprocessing
     preprocessed_collection = preprocess_imagery(
-        image_collection, time_zone, aoi)
+        image_collection, aoi, metadata)
     # 4. Value derivation
-    process_collection(preprocessed_collection)
+    processed_collection = process_collection(preprocessed_collection, aoi)
 
     # 5. Prepare data export
-    prepare_data_export(preprocessed_collection)
+    featureCollection = prepare_export(processed_collection)
 
     # 6. Export data
-    task = create_export_task()
-    task.start()
+    start_export(featureCollection)
     # Perform processing (replace this with your actual logic)
     return "Polygon input is valid and processed."
