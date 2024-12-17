71872a07222ca084b544a3c9bca93eb8ae3c3f8c
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-17 10:26:58 +0100

    feat: add WIP gee export

--- src/gee/task_processing/export.py
+++ src/gee/task_processing/export.py
@@ -1,16 +1,40 @@
 import ee
+from src.gee.task_processing.constants import DAY_ONLY_FEATURE_LABEL
 
 
-def create_export_task(feature_collection: ee.FeatureCollection) -> ee.batch.Export.table.toBigQuery:
+def start_export(feature_collection: ee.FeatureCollection) -> None:
+
+    feature_collection = feature_collection.map(
+        lambda feature: feature.set(
+            'user_id', ee.String(feature_collection.size())).set(
+                "ndvi_polygons", feature.get('ndvi_polygons')))
 
     task = ee.batch.Export.table.toBigQuery(
         collection=feature_collection,
-        # Replace with your BigQuery dataset name
-        table='aiap-436610.comp_gee_data.test',
-        description='Export_NDVI_Mean',
-        append=True,  # Append results to the table
-        selectors=["NDVI_Mean", "geo"],  # Select the NDVI_Mean property
+        table='aiap-436610.comp_gee_data.task_process_result_01',
+        description='Pipeline_version_24',
+        append=True,
+        selectors=[
+            "user_id", "ndvi_polygons"],
     )
 
-    # Start the task
+    # task = ee.batch.Export.table.toBigQuery(
+    #     collection=feature_collection,
+    #     table='aiap-436610.comp_gee_data.task_process_result_01',
+    #     description='Pipeline_version_24',
+    #     append=True,
+    #     selectors=[
+    #         "geo",
+    #         "process_id",
+    #         "user_id",
+    #         "aoi_id",
+    #         "prepared_at",
+    #         "time_zone",
+    #         "image_id",
+    #         "utc_capture_start",
+    #         "utc_capture_end",
+    #         # "ndvi_polygons",
+    #         DAY_ONLY_FEATURE_LABEL
+    #     ],
+    # )
     task.start()
--- src/gee/task_processing/prepare_export.py
+++ src/gee/task_processing/prepare_export.py
@@ -1,8 +1,8 @@
 import ee
+from src.gee.task_processing.constants import DAY_ONLY_FEATURE_LABEL
 
 
 def prepare_export(image_collection: ee.ImageCollection) -> ee.FeatureCollection:
-
     # Flatten the ImageCollection into a FeatureCollection
     flattened_feature_collection = flatten_image_collection(image_collection)
 
@@ -12,43 +12,31 @@ def prepare_export(image_collection: ee.ImageCollection) -> ee.FeatureCollection
     return prepared_feature_collection
 
 
-def extract_combined_polygons(image):
-    combined_polygons = ee.FeatureCollection(image.get('combined_polygons'))
-    return combined_polygons.map(
-        lambda feature: feature.set('image_id', image.get('system:id'))
-    )
-
-# Flatten the ImageCollection into a FeatureCollection
-
-
 def flatten_image_collection(image_collection):
-    combined_polygons_fc = image_collection.map(extract_combined_polygons)
+    combined_polygons_fc = image_collection.map(add_data_to_feature)
     return ee.FeatureCollection(combined_polygons_fc.flatten())
 
-# Prepare FeatureCollection for export
+
+def add_data_to_feature(image):
+    combined_polygons = ee.FeatureCollection(image.get('combined_polygons'))
+    # Convert ndvi_polygons to MultiPolygon and set it as a property
+    updated_combined_polygons = combined_polygons.map(
+        lambda feature: feature.set("process_id", image.get('process_id'))
+        .set("user_id", image.get('user_id'))
+        .set("aoi_id", image.get('aoi_id'))
+        .set("prepared_at", ee.Date(image.get('prepared_at')))
+        .set("time_zone", image.get('time_zone'))
+        .set('image_id', image.get('system:id'))
+        # .set('utc_capture_start', ee.Date(image.get('system:time_start')))
+        # .set('utc_capture_end', ee.Date(image.get('system:time_end')))
+        .set(DAY_ONLY_FEATURE_LABEL, image.get(DAY_ONLY_FEATURE_LABEL))
+        .set('ndvi_polygons', ee.Geometry.MultiPolygon(
+            ee.FeatureCollection(feature.get(
+                'ndvi_polygons')).geometry().geometries()))
+    )
+
+    return updated_combined_polygons
 
 
 def prepare_for_export(feature_collection):
     return feature_collection.select(['geometry', 'ndvi_polygons', 'image_id'])
-
-# Export FeatureCollection to BigQuery or Drive
-
-
-def export_feature_collection(feature_collection, export_type, destination, description):
-    if export_type == 'bigquery':
-        ee.batch.Export.table.toBigQuery(
-            collection=feature_collection,
-            dataset=destination['dataset'],
-            tableName=destination['table_name'],
-            project=destination['project_id'],
-            description=description,
-            selectors=['geometry', 'ndvi_polygons', 'image_id']
-        ).start()
-    elif export_type == 'drive':
-        ee.batch.Export.table.toDrive(
-            collection=feature_collection,
-            description=description,
-            fileFormat='GeoJSON'
-        ).start()
-    print(
-        f"Export started to {export_type}. Monitor progress in the Earth Engine Task Manager.")
