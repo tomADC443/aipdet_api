cf5c8e740b1c9b2fee68e5a0fa3f0b2e112a2a1e
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-17 10:57:25 +0100

    fix: typo in user schema

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -4,19 +4,21 @@ from pydantic import BaseModel, EmailStr, Field
 class UserSignupRequest(BaseModel):
     firstName: str = Field(
         ..., min_length=2, max_length=50,
-        example="John", description="The first name of the user."
+        examples=["John"],
+        description="The first name of the user."
     )
     lastName: str = Field(
         ..., min_length=2, max_length=50,
-        example="Doe", description="The last name of the user."
+        examples=["Doe"],
+        description="The last name of the user."
     )
     email: EmailStr = Field(
-        ..., example="johndoe@example.com",
+        ..., examples=["johndoe@example.com"],
         description="The user's email address."
     )
     password: str = Field(
         ..., min_length=8, max_length=254,
-        example="StrongPassword123",
+        examples=["StrongPassword123"],
         description="The user's password. Must be at least 8 characters long."
     )
 
@@ -26,35 +28,37 @@ class UserSignupRequest(BaseModel):
 class UserLoginRequest(BaseModel):
     email: EmailStr = Field(
         ...,
-        example="johndoe@example.com",
+        examples=["johndoe@example.com"],
         description="The user's email address."
     )
     password: str = Field(
         ...,
         max_length=254,
-        example="StrongPassword123",
+        examples=["StrongPassword123"],
         description="The user's password."
     )
 
 
 class newPasswordDataRequest(BaseModel):
 
-    token: str = Field(...,
-                       min_length=1,
-                       example=1234456,
-                       description="A temporary token to authorize the user to change their password."),
+    token: str = Field(
+        ...,
+        min_length=1,
+        examples=["1234456"],
+        description="A temporary token to authorize the user to change their password."
+    )
 
     userId: str = Field(
 
         ...,
         min_length=1,
-        example="1234-1234-1234-1234",
+        examples=['1234-1234-1234-1234'],
         description="The user's id (as submitted in the email action link)."
     )
 
     password: str = Field(
         ..., min_length=8, max_length=254,
-        example="StrongPassword123",
+        examples=["StrongPassword123"],
         description="The user's password. Must be at least 8 characters long."
     )
 
@@ -63,12 +67,12 @@ class newPasswordDataRequest(BaseModel):
 class UserLoginResponse(BaseModel):
     message: str = Field(
         ...,
-        example="Login successful",
+        examples=["Login successful"],
         description="A message indicating the outcome of the login operation."
     )
     expires: int = Field(
         ...,
-        example="1733308210",
+        examples=["1733308210"],
         description="The date and time when the login token expires as unix timestamp (seconds)."
     )
 
@@ -76,7 +80,7 @@ class UserLoginResponse(BaseModel):
 class ResetPasswordRequest(BaseModel):
     email: EmailStr = Field(
         ...,
-        example="johndoe@example.com",
+        examples=["johndoe@example.com"],
         description="The user's email address."
     )
 
