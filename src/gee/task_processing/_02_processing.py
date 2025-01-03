673bcb2a0c12d994dcdd618bf4410cc0f6270999
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-03 09:28:29 +0100

    refactor: extract constants

--- src/gee/task_processing/_02_processing.py
+++ src/gee/task_processing/_02_processing.py
@@ -1,5 +1,6 @@
 import ee
-from src.gee.task_processing.constants import P
+# P= Property B= Band S= Settings
+from src.gee.task_processing.constants import P, B, S
 from src.gee.task_processing.utils import calculate_masked_percentage
 
 
@@ -7,9 +8,9 @@ def process_collection(image_collection: ee.ImageCollection,  aoi: ee.Geometry) 
 
     def add_bands(image: ee.Image) -> ee.Image:
         image = image.addBands(image.clip(aoi).normalizedDifference(
-            ['B8', 'B4']).rename('NDVI').clip(aoi))
+            ['B8', 'B4']).rename(B["NDVI"]).clip(aoi))
         image = image.addBands(image.select(
-            "NDVI").gt(0.7).rename("NDVI_GT_07"))
+            B["NDVI"]).gt(S["HIGH_NDVI_THRESHOLD"]).rename(B["NDVI_GT_07"]))
 
         return image.updateMask(image.mask())
 
@@ -19,14 +20,15 @@ def process_collection(image_collection: ee.ImageCollection,  aoi: ee.Geometry) 
             geometry=aoi,
             geometryType='polygon',
             reducer=ee.Reducer.count(),
-            scale=10,
+            scale=S["SENTINEL2_SCALE"],  # type: ignore
             bestEffort=False,
             labelProperty=None,
             eightConnected=False,
         )
 
         valid_polygon_feature_collection = valid_polygon_feature_collection.map(
-            lambda feature: feature.simplify(ee.ErrorMargin(20, 'meters'))
+            lambda feature: feature.simplify(ee.ErrorMargin(
+                S["BASIC_SIMPLIFY_TOLERANCE"], S["TOLERANCE_UNIT"]))  # type: ignore
         )
         # mask_percent = calculate_masked_percentage(image, aoi)
         # valid_polygon_feature_collection = valid_polygon_feature_collection.map(
@@ -34,22 +36,23 @@ def process_collection(image_collection: ee.ImageCollection,  aoi: ee.Geometry) 
         # )
 
         valid_polygon_feature_collection = valid_polygon_feature_collection.map(
-            lambda feature: feature.set('image_id', image.id())
+            lambda feature: feature.set(P['image_id'], image.id())
         )
 
         def set_ndvi_area_inside_polygon(feature: ee.Feature) -> ee.Feature:
 
-            ndvi_vectors = image.select("NDVI_GT_07").mask(image.select("NDVI_GT_07").eq(1)).reduceToVectors(
+            ndvi_vectors = image.select(B["NDVI_GT_07"]).mask(image.select(B["NDVI_GT_07"]).eq(1)).reduceToVectors(
                 geometry=feature.geometry(),
                 geometryType='polygon',
                 reducer=ee.Reducer.countEvery(),
-                scale=10,
+                scale=S["SENTINEL2_SCALE"],
                 bestEffort=False,
                 labelProperty=None,
                 eightConnected=False,
             )
             ndvi_vectors = ndvi_vectors.map(
-                lambda feature: feature.simplify(ee.ErrorMargin(20, 'meters'))
+                lambda feature: feature.simplify(ee.ErrorMargin(
+                    S["BASIC_SIMPLIFY_TOLERANCE"], S["TOLERANCE_UNIT"]))  # type: ignore
             )
 
             # Check if collection has features
@@ -58,7 +61,7 @@ def process_collection(image_collection: ee.ImageCollection,  aoi: ee.Geometry) 
             # Set geometry only if features exist
             feature = ee.Feature(ee.Algorithms.If(
                 has_features,
-                feature.set('ndvi_polygons', ndvi_vectors.geometry()),
+                feature.set(P['ndvi_polygons'], ndvi_vectors.geometry()),
                 feature
             ))
 
--- src/gee/task_processing/constants.py
+++ src/gee/task_processing/constants.py
@@ -1,12 +1,27 @@
 
 # Availability of Sentinel-2 imagery starts at 01.04.2017
-NUMBER_OF_DAYS_TEMPORAL_MAX = 10  # 7 years back = 2555 days
+NUMBER_OF_DAYS_TEMPORAL_MAX = 2  # 7 years back = 2555 days
 DAY_ONLY_FEATURE_LABEL = "reduced_local_day"
 
 
 # P = Property (name of a property (e.g. of an image))
 # B = Band (name of a band)
+# S = Settings
 P = {
-    "ndvi_multipolygons": "ndvi_multipolygons",
-    "combined_polygons": "combined_polygons",
+    "ndvi_polygons": "ndvi_polygons",
+    "image_id": "image_id",
+}
+
+B = {
+    "NDVI": "NDVI",
+    "NDVI_GT_07": "NDVI_GT_07"
+}
+
+
+S = {
+    "HIGH_NDVI_THRESHOLD": 0.7,
+    "BASIC_SIMPLIFY_TOLERANCE": 20,
+    "TOLERANCE_UNIT": "meters",
+    "SENTINEL2_SCALE": 10,
+
 }
