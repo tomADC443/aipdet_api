f1899eebbb7975e382b1ec6c05f9e9704b7b086f
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-03 09:29:48 +0100

    feat: add processId

--- src/gee/task_processing/_03_prepare_export.py
+++ src/gee/task_processing/_03_prepare_export.py
@@ -1,8 +1,12 @@
 import ee
 from src.gee.task_processing.constants import DAY_ONLY_FEATURE_LABEL
 from src.gee.task_processing.constants import P
+from src.gee.task_processing.metadata import GeeTaskProcessingMetadata
 
 
-def prepare_export(featureCollection: ee.FeatureCollection) -> ee.FeatureCollection:
+def prepare_export(featureCollection: ee.FeatureCollection, metadata: GeeTaskProcessingMetadata) -> ee.FeatureCollection:
 
+    featureCollection = featureCollection.map(
+        lambda feature: feature.set(P["process_id"], metadata.process_id)
+    )
     return featureCollection
