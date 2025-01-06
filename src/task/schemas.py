d48d377f6d468153bbecadb6ad269834c66cdf50
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-06 14:51:14 +0100

    feat: task deletion schema

--- src/task/schemas.py
+++ src/task/schemas.py
@@ -4,15 +4,23 @@ from pydantic import BaseModel, Field
 class TaskCreationRequest(BaseModel):
     name: str = Field(
         ..., min_length=1, max_length=50,
-        example="My Area of Interest",
+        examples=["My Area of Interest"],
         description="The name of the Task"
     )
     aoiId: str = Field(
         ..., min_length=1, max_length=100,
-        example="fa39a5d2-caf4-4101-8d6d-b1b74f23s83 ",
+        examples=["fa39a5d2-caf4-4101-8d6d-b1b74f23s83 "],
         description="Identifier of the AOI that is associated to this Task"
     )
     isPublic: bool = Field(
         ...,
         description="A Boolean flag specifying if the Current route is pivate or public (can be seen by other users)"
+    )
+
+
+class TaskDeletionRequest(BaseModel):
+    id: str = Field(
+        ..., min_length=1, max_length=100,
+        examples=["1234-47654"],
+        description="The id of the Task that should be deleted."
     )
