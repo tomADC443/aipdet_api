07fd3924d9c6a5a0baa76cd97d6f9acf009bb462
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-06 11:16:55 +0100

    chore: change to 90 days

--- src/gee/task_processing/constants.py
+++ src/gee/task_processing/constants.py
@@ -1,6 +1,6 @@
 
 # Availability of Sentinel-2 imagery starts at 01.04.2017
-NUMBER_OF_DAYS_TEMPORAL_MAX = 2  # 7 years back = 2555 days
+NUMBER_OF_DAYS_TEMPORAL_MAX = 90  # 7 years back = 2555 days
 DAY_ONLY_FEATURE_LABEL = "reduced_local_day"
 
 
