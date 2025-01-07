23b80baf8cb823b90b1638bc57f88de36b374b96
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-07 11:28:37 +0100

    feat: taskId as description for batches

--- src/gee/task_processing/_04_export.py
+++ src/gee/task_processing/_04_export.py
@@ -1,16 +1,18 @@
 import ee
 from src.gee.task_processing.constants import DAY_ONLY_FEATURE_LABEL
 from src.gee.task_processing.constants import P
+from typing import Any
 
 
-def start_export(feature_collection: ee.FeatureCollection) -> None:
+def start_export(feature_collection: ee.FeatureCollection, metadata) -> Any:
 
     task = ee.batch.Export.table.toBigQuery(
         collection=feature_collection,
         table='aiap-436610.comp_gee_data.task_process_result_v2',
-        description='Pipeline_version_test_ohne_classifyer',
+        description=metadata.task_id,
         append=True,
         selectors=[
             '.geo', "image_id", "ndvi_polygons", "user_id", P['process_id'], P['water_hyacinth_classification']]
     )
     task.start()
+    return task
