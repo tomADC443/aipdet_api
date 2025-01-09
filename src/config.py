4c883cf6e44c24338e58898e644eb66093d14417
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-09 16:59:00 +0100

    fix: truncated env

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field  # Add this import
 
 
 class Settings(BaseSettings):
@@ -12,9 +13,9 @@ class Settings(BaseSettings):
     JWT_SECRET: str
     JWT_ALGORITHM: str
     DATABASE_REPORT_TABLE: str
-    AIPDET_FE_SA_GCP: str
-    AIPDET_BE_SA_GCP: str
-    GEE_SA_GCP: str
+    AIPDET_FE_SA_GCP: str = Field(max_length=10000)  # Adjust number as needed
+    AIPDET_BE_SA_GCP: str = Field(max_length=10000)
+    GEE_SA_GCP: str = Field(max_length=10000)
 
 
 @lru_cache()
