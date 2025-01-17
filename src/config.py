b982925fe451ee4c2041ec5a00786b6d156a5ca6
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-17 08:45:13 +0100

    feat: add encoding for env file and add grid table to config

--- src/config.py
+++ src/config.py
@@ -2,10 +2,17 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field  # Add this import
+from pathlib import Path
+
+env_path = Path(__file__).parent.parent / ".env"
+print(f"Env file exists: {env_path.exists()}")
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=str(env_path),
+        env_file_encoding='utf-8'
+    )
     POSTMARK_API_TOKEN: str
     BASE_URL: str
     FRONTEND_BASE_URL: str
@@ -13,6 +20,7 @@ class Settings(BaseSettings):
     JWT_SECRET: str
     JWT_ALGORITHM: str
     DATABASE_REPORT_TABLE: str
+    DATABASE_GRID_TABLE: str
     AIPDET_FE_SA_GCP: str = Field(max_length=10000)  # Adjust number as needed
     AIPDET_BE_SA_GCP: str = Field(max_length=10000)
     GEE_SA_GCP: str = Field(max_length=10000)
