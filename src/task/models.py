da8cfb7faee97cb40cd8b766bbdcec5493bca4f3
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-17 08:51:56 +0100

    feat: add new model for task and gee task relation

--- src/task/models.py
+++ src/task/models.py
@@ -2,6 +2,7 @@ import uuid
 from sqlalchemy import Column, DateTime, ForeignKey, Text, func, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from src.database import Base
+from typing import Union
 
 
 class Task(Base):
@@ -21,3 +22,30 @@ class Task(Base):
 
     def __repr__(self):
         return f"<AOI(id={self.id}, user_id={self.user_id}, name={self.name}, created_at={self.created_at})>"
+
+
+class TaskProcess(Base):
+
+    __tablename__ = "task_processes"
+
+    task_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("task.id", ondelete="CASCADE"),
+        primary_key=True,
+        nullable=False
+    )
+    gee_task_id = Column(
+        Text,
+        primary_key=True,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<TaskProcess(task_id={self.task_id}, gee_task_id={self.gee_task_id})>"
+
+
+def __init__(self, task_id: Union[uuid.UUID, str], gee_task_id: str):
+    if isinstance(task_id, str):
+        task_id = uuid.UUID(task_id)
+    self.task_id = task_id
+    self.gee_task_id = gee_task_id
