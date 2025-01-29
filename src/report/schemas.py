eda3c7d6e9613e9bb15bd79f7b03687b3ebc982a
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-29 12:14:08 +0100

    feat: add input schema for datestring

--- src/report/schemas.py
+++ src/report/schemas.py
@@ -9,3 +9,11 @@ task_id_parameter = Query(
     examples=["1-023948-9182374"],
     description="Id of the task in question."
 )
+dateString = Query(
+    ...,
+    alias="dateString",
+    min_length=10,
+    max_length=10,
+    examples=["2020-05-21"],
+    description="String representation of a date in the format YYYY-MM-DD."
+)
